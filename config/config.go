acc8cf65a25adbdbb731c89c027e4a471ee43799
J. PANUPONG <panupong.pj15@mail.kmutt.ac.th>
2019-08-07 16:31:57 +0700

    [UPDATE] mailer

--- config/config.go
+++ config/config.go
@@ -5,13 +5,15 @@ import (
 )
 
 type Configuration struct {
-	DB_TYPE     string
-	DB_USERNAME string
-	DB_PASSWORD string
-	DB_PORT     string
-	DB_HOST     string
-	DB_NAME     string
-	SECRET      string
+	DB_TYPE       string
+	DB_USERNAME   string
+	DB_PASSWORD   string
+	DB_PORT       string
+	DB_HOST       string
+	DB_NAME       string
+	SECRET        string
+	MAIL_EMAIL    string
+	MAIL_PASSWORD string
 }
 
 func GetConfig() Configuration {
