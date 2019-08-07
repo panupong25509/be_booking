c64eea78ae80bcaa2312a021ce43a51c18e800b3
J. PANUPONG <panupong.pj15@mail.kmutt.ac.th>
2019-08-07 10:33:11 +0700

    [ADD] migration booking table

---
+++ migrations/20190806183207-create_booking_table.sql
@@ -0,0 +1,17 @@
+
+-- +migrate Up
+CREATE TABLE IF NOT EXISTS bookings (
+    id              VARCHAR(36)                 NOT NULL,
+    username        VARCHAR(255)                NOT NULL,
+    password        VARCHAR(255)                NOT NULL,
+    fname           VARCHAR(255)                NOT NULL,
+    lname           VARCHAR(255)                NOT NULL,
+    organization    VARCHAR(255)                NOT NULL,
+    email           VARCHAR(255)                NOT NULL,
+    role            VARCHAR(255)                NOT NULL,
+    create_at       datetime                    NOT NULL,
+    update_at       datetime                    NOT NULL,
+    PRIMARY KEY (id)
+);
+-- +migrate Down
+DROP TABLE bookings;
